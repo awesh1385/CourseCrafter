bd5fb6581fe6b7eac9a883881435c31aefaaa452
AWESH TARAL <122428599+awesh1385@users.noreply.github.com>
2023-06-25 15:55:23 +0530

    login env

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -23,8 +23,8 @@ export const Login = () => {
   }
 
   async function handleLoginGuest() {
-    email.current.value = "awesh@example.com";
-    password.current.value = "Awesh@123";
+    email.current.value = process.env.REACT_APP_GUEST_LOGIN;
+    password.current.value = process.env.REACT_APP_GUEST_PASSWORD;
     try {
       const authDetail = {
         email: email.current.value,
